b9b80d27fb05b64c4955589037dfda2618904384
enigma0884 <sohanshashi0884@gmail.com>
2023-08-15 20:36:31 +0530

    setup signup route

--- src/lib/models/Users.ts
+++ src/lib/models/Users.ts
@@ -1,22 +1,22 @@
 import { Schema, model } from "mongoose";
 
 interface User {
-  username: string
-  password: string
+    username: string
+    password: string
 }
 
 const userSchema = new Schema<User>({
-	username: { type: String, required: true, unique: true },
-	password: { type: String, required: true }
+    username: { type: String, required: true, unique: true, index: true },
+    password: { type: String, required: true }
 
 }, {
-	timestamps: true
+    timestamps: true
 });
 
 userSchema.virtual("bookings", {
-	localField: "_id",
-	foreignField: "userID",
-	ref: "Bookings"
+    localField: "_id",
+    foreignField: "userID",
+    ref: "Bookings"
 });
 
 userSchema.set("toJSON", { virtuals: true });
--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,3 +1,30 @@
-<h1>This is the signup page</h1>
+<script lang="ts">
+    import type { ActionData } from "./$types";
+    export let form: ActionData;
+</script>
 
-<!-- component level user logic state here -->
+<form method="POST">
+    <label>
+        Username
+        <input type="text" name="username" />
+    </label>
+
+    <label>
+        Password
+        <input type="password" name="password" />
+    </label>
+
+    <button>Sign up</button>
+</form>
+
+{#if form?.usernameMissing}
+    <p>Username cannot be empty</p>
+{:else if form?.passwordMissing}
+    <p>Password cannot be empty</p>
+{:else if form?.passwordLength}
+    <p>Passwords must be 8 characters or longer</p>
+{:else if form?.userExisting}
+    <p>
+        That user already exists. Do you wish to <a href="/login">login</a> instead?
+    </p>
+{/if}
